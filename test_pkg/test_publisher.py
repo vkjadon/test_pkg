3e48d89b40368edb5e5460c58174c5ac19924843
vkjadon <vijay.jadon@gmail.com>
2024-07-28 10:07:04 +0530

    Preparing to push from local

---
+++ test_pkg/test_publisher.py
@@ -0,0 +1,42 @@
+#Commented
+import sys
+
+import rclpy
+from rclpy.executors import ExternalShutdownException
+from rclpy.node import Node
+
+from std_msgs.msg import String
+
+
+class MinimalPublisher(Node):
+
+    def __init__(self):
+        super().__init__('publisher')
+        self.publisher_ = self.create_publisher(String, 'topic', 10)
+        timer_period = 0.5  # seconds
+        self.timer = self.create_timer(timer_period, self.timer_callback)
+        self.i = 0
+
+    def timer_callback(self):
+        msg = String()
+        msg.data = 'Hello World: %d' % self.i
+        self.publisher_.publish(msg)
+        self.get_logger().info('Publishing: "%s"' % msg.data)
+        self.i += 1
+
+
+def main(args=None):
+    rclpy.init(args=args)
+
+    try:
+        minimal_publisher = MinimalPublisher()
+
+        rclpy.spin(minimal_publisher)
+    except KeyboardInterrupt:
+        pass
+    except ExternalShutdownException:
+        sys.exit(1)
+
+
+if __name__ == '__main__':
+    main()
